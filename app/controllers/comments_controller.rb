4745b05afee0f7be09f50d604851c73db308dd59
Felix <felix_hb94@hotmail.com>
2016-05-02 16:52:13 +0000

    nothing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -44,7 +44,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to "/contributions/"+@comment.contribution_id }
+        format.html { redirect_to "/contributions/#{@comment.contribution_id}" }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }

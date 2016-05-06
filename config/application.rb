c85d8a2b4f915fc78041ed3e25340a1a41373e0b
Felix <felix_hb94@hotmail.com>
2016-05-06 11:24:22 +0000

    Undoing CORS

--- config/application.rb
+++ config/application.rb
@@ -22,23 +22,6 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     
-    config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
-     allow do
-       origins '*'
-
-       resource '/cors',
-         :headers => :any,
-         :methods => [:post],
-         :credentials => true,
-         :max_age => 0
-
-       resource '*',
-         :headers => :any,
-         :methods => [:get, :post, :delete, :put, :patch, :options, :head],
-         :max_age => 0
-     end
-   end
-   
     config.active_record.raise_in_transactional_callbacks = true
   end
 end

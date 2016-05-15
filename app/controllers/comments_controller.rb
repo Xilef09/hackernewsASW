b169294bc712614f95c6c78a0595b9076f630454
Felix <felix_hb94@hotmail.com>
2016-05-15 20:07:39 +0000

    get comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -31,6 +31,7 @@ class CommentsController < ApplicationController
   # GET /comments/1.json
   def show
     @comment = Comment.find(params[:id])
+    @replies = Reply.where(comment_id: @comment.id)
   end
 
   # GET /comments/new
--- app/views/comments/show.json.jbuilder
+++ app/views/comments/show.json.jbuilder
@@ -1,1 +1,2 @@
 json.extract! @comment, :id, :content, :puntos, :user_id , :contribution_id, :created_at, :updated_at
+json.extract! @replies, :id

3c3bab171d204da88bdfc39c61fa9f00a11e12c0
Felix <felix_hb94@hotmail.com>
2016-05-16 18:47:26 +0000

    testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
         format.json { render :show, location: @contributions}
       elsif params[:submission_type] == "comments"
         @comments = Comment.where(user_id: params[:user_id]).order(created_at: :desc)
-        format.json {render :show, location: @commennts}
+        format.json {render :show, location: @comments}
       elsif params[:submission_type] == "replies"
         @replies = Reply.where(user_id: params[:user_id]).order(created_at: :desc)
         format.json { render :show, location: @replies}

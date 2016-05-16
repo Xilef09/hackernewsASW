f7f2028f7d96874a0fa6310bb8098de6958a1234
Felix <felix_hb94@hotmail.com>
2016-05-16 19:42:58 +0000

    Funciona?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,23 +14,22 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user  = User.find(params[:id])
+    #@user  = User.find(params[:id])
     #respond_to do |format|
-      if params[:submission_type] == "contributions"
-        @contributions = Contribution.where(user_id: @user.id).order(created_at: :desc)
-        #format.json { render :show, status: :ok, location: @contributions}
-      elsif params[:submission_type] == "comments"
-        @comments = Comment.where(user_id: @user.id).order(created_at: :desc)
-        render 'showcomments.json'
+    #  if params[:submission_type] == "contributions"
+     #   @contributions = Contribution.where(user_id: @user.id).order(created_at: :desc)
+      #  #format.json { render :show, status: :ok, location: @contributions}
+    #  elsif params[:submission_type] == "comments"
+     #   @comments = Comment.where(user_id: @user.id).order(created_at: :desc)
+    #    render 'showcomments.json'
         #format.json {render :showcomments,  status: :ok, location: @user}
-      elsif params[:submission_type] == "replies"
-        @replies = Reply.where(user_id: @user.id).order(created_at: :desc)
+     # elsif params[:submission_type] == "replies"
+      #  @replies = Reply.where(user_id: @user.id).order(created_at: :desc)
         #format.json { render :show, status: :ok, location: @replies}
-      else 
+      #else 
         #format.json { render :show, status: :created, location: @user }
-      end
+      #end
     #end
-    
   end
 
   # GET /users/1/contributions
--- app/views/users/show.json.jbuilder
+++ app/views/users/show.json.jbuilder
@@ -1,2 +1,5 @@
 json.extract! @user, :id, :name, :created_at, :karma, :about, :email
 json.hal user_url(@user)
+json.array!(@comments) do |comment|
+  json.extract! comment, :id, :content, :puntos, :created_at, :user_id, :contribution_id
+  json.hal comment_url(comment)

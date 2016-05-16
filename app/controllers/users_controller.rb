746f85e354aed99436af3b3d548ec492e8c450b3
Felix <felix_hb94@hotmail.com>
2016-05-16 15:05:53 +0000

    PUT user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,14 +66,16 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    #@user =  User.find(decodeToken(params[:user_token]))
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+    id = decodeToken(params[:user_token])
+    if (@user.id == id)
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to @user, notice: 'User was successfully updated.' }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :edit }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
